c094c1c5daa5b24dafad37a1bb24de036927412c
Shiva Kumar (SW-CLOUD) <shivaku@nvidia.com>
2025-06-26 21:40:53 +0530

    auto nvcr image update
    Signed-off-by: Shiva Kumar (SW-CLOUD) <shivaku@nvidia.com>

---
+++ .github/renovate.js
@@ -0,0 +1,20 @@
+module.exports = {
+  hostRules: [
+    {
+      hostType: "docker",
+      matchHost: "nvcr.io",
+      username: "$oauthtoken",
+      password: "***********"
+    }
+  ],
+  customManagers: [
+    {
+      customType: "regex",
+      managerFilePatterns: ["/^deployments/gpu-operator/.*\\.ya?ml$/"],
+      matchStrings: [
+        "image:\\s*\"?(?<depName>[^:@\"]+)(?::(?<currentValue>[\\w.\\-]+))?\"?"
+      ],
+      datasourceTemplate: "docker"
+    }
+  ]
+};
